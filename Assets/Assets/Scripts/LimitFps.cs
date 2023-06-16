03edb526034456b8f448aca0a285ef75df410aaf
Tran Tuan Anh <trantuananh4122@gmail.com>
2023-06-16 15:03:14 +0700

    update again menu ui and update smoke effect  function fixing bullet

---
+++ Assets/Assets/Scripts/LimitFps.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LimitFps : MonoBehaviour
+{
+     void Start()
+   {
+        Application.targetFrameRate = 60;
+  
+   }
+}
