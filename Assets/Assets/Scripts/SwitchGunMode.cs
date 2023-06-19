c004d6fe5ab1e4a6bba280eb59c780ededca24e8
Tran Tuan Anh <trantuananh4122@gmail.com>
2023-06-19 11:35:01 +0700

    update mode burst gun

---
+++ Assets/Assets/Scripts/SwitchGunMode.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SwitchGunMode : MonoBehaviour
+{
+
+    public Gun gunMode;
+
+    public void isBurst( )
+    {
+        gunMode.IsBurst(3);
+        Debug.Log("Switch");
+    }
+
+
+}
